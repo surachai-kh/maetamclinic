67990c33d9d0c439ffb90229625e24c4535e8e2b
surachai <surachai49157@gmail.com>
2021-06-16 15:08:14 +0700

    profile โง่ๆ

--- src/app/main/components/home/home.component.ts
+++ src/app/main/components/home/home.component.ts
@@ -12,7 +12,6 @@ import { Label } from 'ng2-charts';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-
   //ข้อมูลผู้ใช้ที่ login
   userLogin!: firebase.User;
 
@@ -20,44 +19,12 @@ export class HomeComponent implements OnInit, OnDestroy {
     private app: AppService,
     private auth: AngularFireAuth,
     private router: Router
-  ) {
-    this.loadUserLogin();
-  }
+  ) {}
 
   ngOnDestroy() {
   }
 
   ngOnInit() {
-
-  }
-
-
-  //ออกจากระบบ
-  onLogout() {
-    this.app.loading(true);
-    this.auth.signOut()
-      .then(() => {
-        this.app.successAlert('ออกจากระบบแล้ว')
-        this.router.navigate(['/authentication/login'])
-      })
-      .catch(error => this.app.errorAlert(error.message))
-      .finally(() => this.app.loading(false))
-  }
-
-  //ยืนยันอีเมล์
-  onVerifyEmail() {
-    this.app.loading(true);
-    this.userLogin.sendEmailVerification()
-      .then(() => this.app.successAlert('ส่งการยืนยันไปที่อีเมล์แล้ว'))
-      .catch(error => this.app.errorAlert(error.message))
-      .finally(() => this.app.loading(false))
-  }
-
-  //load data login user
-  private loadUserLogin() {
-    this.auth.user.subscribe(user => {
-      if (user) this.userLogin = user;
-    });
   }
 
   //Bar chart
